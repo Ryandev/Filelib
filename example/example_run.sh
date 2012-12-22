5b3606ca0ae08310931354c2d0c0dec8b5bfc193
Ryan Powell <ryan@gorillaapps.co.uk>
2012-12-22 23:35:15 +0000

    Added inc folder

--- example/example_run.sh
+++ example/example_run.sh
@@ -1,2 +1,2 @@
-gcc -std=c99 example_main.c ../src/Filelib.c ../src/Filelib_util.c -I ../src -o file_example
+gcc -std=c99 example_main.c ../src/Filelib.c ../src/Filelib_util.c -I ../inc -I ../src -o file_example
 ./file_example
